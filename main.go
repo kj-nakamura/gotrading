d13aa4b271680743bf83fb7a9098fb70bb75a5c4
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2020-06-17 10:16:22 +0900

    テスト追加

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"gotrading/app/controllers"
 	"gotrading/config"
 	"gotrading/utils"
@@ -11,4 +12,11 @@ func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	controllers.StreamIngestionData()
 	log.Println(controllers.StartWebServer())
+}
+
+func example(code string) (int, error) {
+	if code == "hoge" {
+		return 1, nil
+	}
+	return 0, errors.New("code must be hoge")
 }
