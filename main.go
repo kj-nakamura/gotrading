2fa94d698223ec1bdc0c089b98db85a990347222
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2020-06-17 13:05:09 +0900

    config使わない

--- main.go
+++ main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
-	"gotrading/app/controllers"
+	"encoding/json"
+	"fmt"
+
 	"log"
+	"net/http"
 )
+
+type HealthCheck struct {
+	Status int
+	Result string
+}
 
 func main() {
 	// utils.LoggingSettings(config.Config.LogFile)
 	// controllers.StreamIngestionData()
-	log.Println(controllers.StartWebServer())
+	log.Println(StartWebServer())
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	ping := HealthCheck{http.StatusOK, "ok"}
+
+	res, err := json.Marshal(ping)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
+func StartWebServer() error {
+	http.HandleFunc("/health-check/", healthCheckHandler)
+
+	return http.ListenAndServe(fmt.Sprintf(":%d", 8080), nil)
 }
