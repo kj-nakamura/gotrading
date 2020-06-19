9a952888097deb3565495c7d2acc6bf07a4b7dc7
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2020-06-19 10:02:45 +0900

    戻し

--- Dockerfile
+++ Dockerfile
@@ -9,12 +9,12 @@ WORKDIR /go/src/gotrading
 # ホストのファイルをコンテナの作業ディレクトリに移行
 ADD ./ /go/src/gotrading
 
-# RUN go get github.com/go-sql-driver/mysql
-# RUN go get -u github.com/jinzhu/gorm
-# RUN go get github.com/gorilla/websocket
-# RUN go get github.com/markcheno/go-talib
-# RUN go get golang.org/x/sync/semaphore
-# RUN go get gopkg.in/ini.v1
+RUN go get github.com/go-sql-driver/mysql
+RUN go get -u github.com/jinzhu/gorm
+RUN go get github.com/gorilla/websocket
+RUN go get github.com/markcheno/go-talib
+RUN go get golang.org/x/sync/semaphore
+RUN go get gopkg.in/ini.v1
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o /go/src/gotrading
 
 EXPOSE 8090
--- main.go
+++ main.go
@@ -1,40 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"gotrading/app/controllers"
+	"gotrading/config"
+	"gotrading/utils"
 
 	"log"
-	"net/http"
 )
 
-type HealthCheck struct {
-	Status int
-	Result string
-}
-
 func main() {
-	// utils.LoggingSettings(config.Config.LogFile)
-	// controllers.StreamIngestionData()
-	log.Println(StartWebServer())
-}
-
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	ping := HealthCheck{http.StatusOK, "ok"}
-
-	res, err := json.Marshal(ping)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(res)
-}
-
-func StartWebServer() error {
-	http.HandleFunc("/health-check/", healthCheckHandler)
-
-	return http.ListenAndServe(fmt.Sprintf(":%d", 8090), nil)
+	utils.LoggingSettings(config.Config.LogFile)
+	controllers.StreamIngestionData()
+	log.Println(controllers.StartWebServer())
 }
